4601f584a4354c5c7133c4abb4cb5405f5da046a
Felipe Kauan <99798470+ShadicGamesBr@users.noreply.github.com>
2023-07-06 03:06:08 -0300

    sons

---
+++ scripts/JS.js
@@ -0,0 +1,15 @@
+
+const action = document.getElementById("action")
+var bot = document.getElementsByTagName("p");
+
+action.addEventListener("click", clicar)
+function clicar(){
+    window.alert(bot.length)
+}
+
+for(i = 0; i < bot.length ; i++){
+    bot[i].addEventListener("click", function(){
+        const audio1 = new Audio("sons/click.mp3");
+        audio1.play()
+    })
+}
