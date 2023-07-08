5ae00a08cb944f411da817e687621c4729303c3c
Felipe Kauan <99798470+ShadicGamesBr@users.noreply.github.com>
2023-07-07 22:14:45 -0300

    comeco

--- scripts/JS.js
+++ scripts/JS.js
@@ -1,6 +1,7 @@
 
 const action = document.getElementById("action")
 var bot = document.getElementsByTagName("p");
+const audio1 = new Audio("sons/click.mp3");
 
 action.addEventListener("click", clicar)
 function clicar(){
@@ -8,7 +9,7 @@ function clicar(){
 
 for(i = 0; i < bot.length ; i++){
     bot[i].addEventListener("click", function(){
-        const audio1 = new Audio("sons/click.mp3");
+        
         audio1.play()
     })
 }
